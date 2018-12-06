51e362babcba1c4d0e70edfca67ecae73c3a3006
moqinggen <mo1056610188@163.com>
2018-12-06 17:30:06 +0800

    退出登录

---
+++ src/main/java/com/tss/authserver/controller/OAuth2Controller.java
@@ -0,0 +1,51 @@
+package com.tss.authserver.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/oauth2")
+public class OAuth2Controller {
+    private static final Logger LOG = LoggerFactory.getLogger(OAuth2Controller.class);
+
+    @Autowired
+    private ConsumerTokenServices tokenServices;
+
+    /**
+     * 当前登陆用户信息<br>
+     * <p>
+     * security获取当前登录用户的方法是SecurityContextHolder.getContext().getAuthentication()<br>
+     * 返回值是接口org.springframework.security.core.Authentication，又继承了Principal<br>
+     * 这里的实现类是org.springframework.security.oauth2.provider.OAuth2Authentication<br>
+     * <p>
+     * 因此这只是一种写法，下面注释掉的三个方法也都一样，这四个方法任选其一即可，也只能选一个，毕竟uri相同，否则启动报错<br>
+     * 2018.05.23改为默认用这个方法，好理解一点
+     *
+     * @return
+     */
+    @GetMapping("/user-me")
+    public Authentication principal() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        LOG.info("user-me:{}", authentication.getName());
+        return authentication;
+    }
+
+    
+    /**
+     * 注销登陆/退出
+     * 移除access_token和refresh_token<br>
+     * 用ConsumerTokenServices，该接口的实现类DefaultTokenServices已有相关实现
+     *
+     * @param access_token
+     */
+    @PostMapping(value = "/remove_token")
+    public void removeToken(@RequestParam("access_token") String access_token) {
+        tokenServices.revokeToken(access_token);
+    }
+
+}
