20b78d7529e87113848665cd87e184ea55d24f6a
moqinggen <mo1056610188@163.com>
2018-12-07 17:05:27 +0800

    获取登录用户基本信息

--- src/main/java/com/tss/authserver/controller/OAuth2Controller.java
+++ src/main/java/com/tss/authserver/controller/OAuth2Controller.java
@@ -1,5 +1,6 @@
 package com.tss.authserver.controller;
 
+import com.tss.basic.site.user.annotation.UserAuthInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,15 @@ public class OAuth2Controller {
      * @return
      */
     @GetMapping("/user-me")
-    public Authentication principal() {
+    public UserAuthInfo principal() {
+        UserAuthInfo userAuthInfo = new UserAuthInfo();
+        
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        LOG.info("user-me:{}", authentication.getName());
-        return authentication;
+        if (authentication != null) {
+            userAuthInfo.setUserAcc(authentication.getName());
+            return userAuthInfo;
+        }
+        return userAuthInfo;
     }
 
     
